8880d9925875f80bcee300da52b8235ca50435fa
ADESI\lvonhaef <lukas.von.haefen@adp.com>
2024-12-20 11:53:13 +0100

    added minimum App for first page

--- DnD_Django/game/models.py
+++ DnD_Django/game/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class Empty(models.Model):
+    pass
--- DnD_Django/game/urls.py
+++ DnD_Django/game/urls.py
@@ -5,4 +5,5 @@ from game import views
 app_name = "game"
 urlpatterns = [
      path("", views.IndexView.as_view(), name="index"),
+     # path('simple/', views.simple_view, name='simple_view'),s
 ]
