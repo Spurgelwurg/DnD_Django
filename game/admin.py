5e899d38823c97517e4270d735825c145036c6a4
ADESI\lvonhaef <lukas.von.haefen@adp.com>
2025-03-14 11:13:32 +0100

    register DnDUser model and enhance login/register forms with error handling

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from game.models import DnDUser
+
 # Register your models here.
+admin.site.register(DnDUser)
--- game/forms.py
+++ game/forms.py
@@ -9,15 +9,13 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
     
 class RegisterForm(UserCreationForm):
-    role = forms.ChoiceField(choices=DnDUser.ROLE_CHOICES)
-
     class Meta:
         model = DnDUser
-        fields = ['username', 'email', 'role']
+        fields = ['username', 'email']
     
     # Reroder the default form fields
     def __init__(self, *args, **kwargs):
         super(RegisterForm, self).__init__(*args, **kwargs)
         keys = list(self.fields.keys())
-        new_order = keys[0:1] + keys[-2:] + keys[1:-2]
+        new_order = keys[0:1] + keys[-1:] + keys[1:-1]
         self.fields = {key: self.fields[key] for key in new_order}
